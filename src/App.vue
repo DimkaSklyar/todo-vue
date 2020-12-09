779130a1b4dde1d07756ca28acaa5e21ed8a199c
Dmitry <dimkasklyar@mail.ru>
2020-12-09 16:54:11 +0300

    v.0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-app">
+      <aside class="sidebar">
+        <List
+          :lists="allListTasks"
+          :activeItem="activeItem"
+          @on-click="handleActiveItem"
+        />
+        <List
+          :lists="lists"
+          :activeItem="activeItem"
+          @on-click="handleActiveItem"
+        />
+      </aside>
+      <div class="tasklist"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import List from "./components/List.vue";
+import menuImage from './assets/img/menu.png';
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      lists: [],
+      activeItem: null,
+      allListTasks: [{
+        name: 'Все задачи',
+        icon: menuImage
+      }]
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3004/lists?_expand=color&_embed=tasks")
+      .then((resp) => (this.lists = resp.data));
+  },
+  methods: {
+    handleActiveItem: function (id) {
+      this.activeItem = id;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    List,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  font-weight: normal;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+.container-app {
+  display: flex;
+  width: 750px;
+  min-height: 530px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+}
+
+.sidebar {
+  background: #e5e5e5;
+  width: 200px;
+  border-right: 1px solid #f1f1f1;
+  padding: 20px;
+}
+
+.tasklist {
+  background: #fff;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul class="list">
+    <li
+      v-for="list in lists"
+      :key="list.id"
+      :class="['list__item', { active: activeItem === list.id }]"
+      @click="$emit('on-click', list.id)"
+    >
+      <i
+        v-if="!!list.color"
+        class="list__item__icon"
+        :style="{ background: list.color.hex }"
+      ></i>
+      <i v-else>
+        <img :src="list.icon" alt="" />
+      </i>
+      {{ list.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    lists: Array,
+    activeItem: Number || null,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  list-style: none;
+
+  &__item {
+    padding: 12px 10px;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 4px;
+
+    &.active {
+      background: #fff;
+    }
+
+    &__icon {
+      border-radius: 50%;
+      width: 10px;
+      height: 10px;
+      display: inline-block;
+    }
+    i {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
